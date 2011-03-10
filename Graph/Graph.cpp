6a9312e1982a49df9b83eb709857ced7b7dff061
Kiel <kiel.friedt@gmail.com>
2011-03-10 00:35:44 -0800

    modified:   Graph.cpp 	modified:   Graph.h 	modified:   main.cpp

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -1,3 +1,4 @@
+
 /*
  *	Copyright (C) 2011  Kiel Friedt
  *
--- Graph/main.cpp
+++ Graph/main.cpp
@@ -1,4 +1,4 @@
-/*
+ /*
  *	Copyright (C) 2011  Kiel Friedt
  *
  *    This program is free software: you can redistribute it and/or modify
